a0703167ddc9dfc44bc2988b027d3b0a0d4487c2
오진혁 <hyeok7777@gmail.com>
2021-10-23 22:54:40 +0900

    RequestBodyJsonServlet.java

---
+++ src/main/java/hello3/servlet/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello3.servlet.basic;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
--- src/main/java/hello3/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello3/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,6 +1,9 @@
 package hello3.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello3.servlet.basic.HelloData;
 import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
@@ -11,18 +14,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
-public class RequestBodyStringServlet extends HttpServlet {
+@WebServlet(name="requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         ServletInputStream inputStream = request.getInputStream();
-
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-
         System.out.println("messageBody = " + messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
 
-        response.getWriter().write("ok");
-
+        System.out.println("helloData = " + helloData.getUsername());
+        System.out.println("helloData = " + helloData.getAge());
     }
 }
