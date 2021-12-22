6198b53c676b8c13c88a422381f63372664d56ec
오진혁 <hyeok7777@gmail.com>
2021-12-22 23:23:21 +0900

    Default Changelist

--- src/main/java/hello3/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello3/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import hello3.servlet.web.frontcontroller.v3.ControllerV3;
 import hello3.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello3.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello3.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello3.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello3.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello3.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello3.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello3.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,10 +38,15 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
@@ -61,7 +70,7 @@ public class FrontControllerServletV5 extends HttpServlet {
     }
 
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
-        MyHandlerAdapter myHandlerAdapter;
+
         for (MyHandlerAdapter adapter : handlerAdapters) {
             if (adapter.supports(handler)) {
                 return adapter;
